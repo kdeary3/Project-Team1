fcd50675356875cd6665b2b1209210fbd98cd167
kdeary3 <kenodeary3@gmail.com>
2026-04-20 15:15:23 -0500

    first commit

---
+++ src/main/java/mil/t2com/moda/ratemyboss/RatemybossApplication.java
@@ -0,0 +1,13 @@
+package mil.t2com.moda.ratemyboss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatemybossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatemybossApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mil/t2com/moda/ratemyboss/RatemybossApplicationTests.java
@@ -0,0 +1,13 @@
+package mil.t2com.moda.ratemyboss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatemybossApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
