1b0a5f46f675af824cc9de374158aa3cfd0c9bf7
kdeary3 <kenodeary3@gmail.com>
2026-04-24 10:45:23 -0500

    added sample data

---
+++ src/main/resources/db/migration/V3__.sql
@@ -0,0 +1,11 @@
+INSERT INTO leader (first_name, last_name, job_title) VALUES
+                                                          ('Michael', 'Scott', 'Regional Manager'),
+                                                          ('Miranda', 'Priestley', 'Editor in Chief'),
+                                                          ('Ted', 'Lasso', 'Head Coach'),
+                                                          ('Leslie', 'Knope', 'Deputy Director of Parks and Rec'),
+                                                          ('Captain', 'Raymond Holt', 'Police Captain'),
+                                                          ('Shiv', 'Roy', 'President of Domestic Operations'),
+                                                          ('Gregory', 'House', 'Head of Diagnostic Medicine'),
+                                                          ('Ron', 'Swanson', 'Director of Parks and Rec'),
+                                                          ('Olivia', 'Pope', 'Crisis Manager'),
+                                                          ('Tony', 'Soprano', 'Waste Management Consultant');
