269a9fb5acf4d1c6a706851f92f177dd6c25daa6
wyatt.t.putnam.mil@army.mil <wyatt.t.putnam.mil@army.mil>
2026-04-30 15:26:45 -0500

    Added Leader Dropdown search feature

--- frontend/app/components/LeadersDropdown.tsx
+++ frontend/app/components/LeadersDropdown.tsx
@@ -1,37 +1,67 @@
 import {useEffect, useState} from "react";
-import type {Leader} from "~/leader/LeaderType";
+import type {LeaderType} from "~/leader/LeaderType";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faChevronDown} from "@fortawesome/free-solid-svg-icons"
 
 const LeadersDropdown = ({onLeaderSelect}: any) => {
 
     const [filterLeader, setFilterLeader] = useState("ALL")
-    const [leaders, setLeaders] = useState<Leader[]>([])
-    const [selectLeader, setSelectLeader] = useState("")
+    const [filteredLeaders, setFilteredLeaders] = useState<LeaderType[]>([]);
+    const [leaders, setLeaders] = useState<LeaderType[]>([])
+    const [isHidden, setIsHidden] = useState<boolean>(true);
+    const [selectLeader, setSelectLeader] = useState<number>(-1)
+    const [name, setName] = useState<string>("");
 
     const url = 'http://localhost:8080'
 
     useEffect(() => {
         fetch(`${url}/api/leaders`)
             .then(res => res.json())
-            .then(data => setLeaders(data))
+            .then(data => {
+                setLeaders(data)
+                setFilteredLeaders(data);
+            })
     }, []);
 
-    const filteredLeaders = filterLeader === "ALL"
-        ? leaders
-        : leaders.filter(leader => leader.id.toString() === filterLeader)
-    console.log(filteredLeaders)
+
+
+    // const filteredLeaders = filterLeader === -1
+    //     ? leaders
+    //     : leaders.filter(leader => leader.id.toString() === filterLeader)
+    // console.log(filteredLeaders)
+
+    const setFilter = (leaderName : string) =>{
+        setName(leaderName);
+        if (leaderName === "" || leaderName === undefined || leaderName === "ALL"){
+            setFilteredLeaders(leaders);
+            return;
+        }
+        setFilteredLeaders(leaders.filter(leader => (`${leader.firstName}, ${leader.lastName}`).toUpperCase().indexOf(leaderName.toUpperCase()) > -1));
+    }
+
+    const showDropDown = () => {
+        setIsHidden(!isHidden);
+    }
+
+    const handleClick = (leaderIdString : string) =>{
+        setFilter("ALL");
+        onLeaderSelect(leaderIdString);
+        showDropDown();
+        setName("");
+    }
 
     return (
-        <div>
-            <label htmlFor="leaderSelect">
-                <select onChange={(e) => onLeaderSelect(e.target.value)}>
-                    <option value="ALL">All Leaders</option>
-                    {leaders.map((leader) => (
-                        <option key={leader.id} value={leader.id}>
-                            {leader.firstName}
-                        </option>
-                    ))}
-                </select>
-            </label>
+        <div onMouseLeave={!isHidden ? showDropDown : () => {}} className={"relative flex flex-col bg-gray-400 rounded-t w-50"}>
+            <button className={"cursor-pointer rounded-t hover:bg-gray-300 hover:rounded-t flex flex-row p-2 justify-between items-center"} onClick={showDropDown}>Dropdown <FontAwesomeIcon icon={faChevronDown} /></button>
+            <div hidden={isHidden} className={"absolute w-full mt-10 bg-gray-400 b-rounded p-2"}>
+                <div className={"flex flex-col"}>
+                    <input className={"bg-white h-fit rounded flex flex-row justify-between"} type="text" value={name} onChange={e => setFilter(e.target.value)}/>
+                    <a onClick={() => handleClick("ALL")} className={"hover:cursor-pointer hover:bg-gray-300"}>ALL</a>
+                    {filteredLeaders.map((leader) => (
+                        <a onClick={() => handleClick(leader.id.toString())} key={leader.id} className={"hover:cursor-pointer hover:bg-gray-300"}>{`${leader.firstName}, ${leader.lastName}`}</a>)
+                    )}
+                </div>
+            </div>
         </div>
     );
 };
