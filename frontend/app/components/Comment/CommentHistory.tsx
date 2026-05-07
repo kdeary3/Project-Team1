66bc5b874bd80b895760279ee6a2ca8ffc68bcb9
James Larimer <james.l.larimer.mil@army.mil>
2026-05-07 09:39:13 -0500

    Add font awesome thumbs up and down

--- frontend/app/components/Comment/CommentHistory.tsx
+++ frontend/app/components/Comment/CommentHistory.tsx
@@ -1,4 +1,5 @@
-
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faThumbsDown, faThumbsUp} from "@fortawesome/free-solid-svg-icons";
 
 type CommentHistoryProps = {
     comment: String;
@@ -9,10 +10,27 @@ type CommentHistoryProps = {
 const CommentHistory = ({comment, created_at} : CommentHistoryProps)  => {
     return (
 
-        <div className="flex flex-col w-11/12 h-65 my-2 p-3 mb-8 border-2 rounded-xl border-black">
-            <p>{comment}</p>
+        <div className="flex flex-col w-11/12 h-50 my-5 p-5 border-2 rounded-xl border-blue-900 bg-slate-200">
 
-            <p>{created_at}</p>
+            {/* Comment Area */}
+            <div className="mt-3 space-y-3 px-4">
+
+                <p className="p-2 px-4 h-25 border border-slate-800 rounded-xl bg-slate-100 text-start text-blue-800 text-lg focus:outline-none">{comment}</p>
+
+                <div className="flex justify-between">
+                    <p className="text-blue-800 text-lg font-medium">{created_at}</p>
+
+                    <div>
+                        <FontAwesomeIcon icon={faThumbsUp} className="text-3xl hover:text-green-500 hover:rotate-5 px-2" />
+                        <FontAwesomeIcon icon={faThumbsDown} className="text-3xl hover:text-red-500 hover:rotate-360 duration-500 px-2" />
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
         </div>
     )
 }
--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -23,6 +23,11 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+
+  {
+    rel: "preconnect",
+    href:"https://kit.fontawesome.com/db4cecc8dc.js"
+  }
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
